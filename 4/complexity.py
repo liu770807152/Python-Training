1dbae125ba2d2b7a4fecfd6069aa35c184caca6a
Bradley <liu770807152@hotmail.com>
2021-04-12 00:56:35 +1000

    Improve codes of complexity.

--- 4/complexity.py
+++ 4/complexity.py
@@ -40,18 +40,3 @@ def fib(n):
 for i in range(200):
     print(i, fib(i))
 print('Done!')
-
-'''
-以下程序打印前200个斐波那契数列, 带缓存
-'''
-from functools import cache, lru_cache
-# @cache
-@lru_cache(maxsize=5)
-def fib(n):
-    if n <= 1:
-        return n
-    return fib(n - 1) + fib(n - 2)
-
-for i in range(200):
-    print(i, fib(i))
-print('Done!')
