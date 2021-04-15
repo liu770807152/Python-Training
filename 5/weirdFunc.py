a7c49589448c37608f2e40b445460b991005f241
Bradley <liu770807152@hotmail.com>
2021-04-15 22:04:25 +1000

    Added codes for weirdFunc.py

--- 5/weirdFunc.py
+++ 5/weirdFunc.py
@@ -1,19 +1,27 @@
 '''
 函数的参数传递到底是传值，还是传引用？
 '''
-def weird_func_1(param):
+def weird_func_1(param: str):
     param = param + '.'
+    param = param.replace('i', 'I', 1)
     return param
 
 
-def weird_func_2(param):
+def weird_func_2(param: list):
     param.append(0)
     return param
 
+'''
+函数没有return语句，就真的没有返回值吗？
+'''
+def no_return_func(first = 0, second = 0):
+    sum = first + second
+
 if __name__ == '__main__':
-    s = 'I am a string'
+    s = 'i am a string'
     l = [3, 2, 1]
     print('Return result: ' + weird_func_1(s))
     print('After execution: ' + s)
-    print('Return result: ' + weird_func_2(l))
-    print('After execution: ' + l)
+    print('Return result: ' + str(weird_func_2(l)))
+    print('After execution: ' + str(l))
+    print('The return value of no_return_func is: ' + str(no_return_func(1, 2)))
