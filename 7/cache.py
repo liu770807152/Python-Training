4c97cd6a0283b8b292a76bc3be3516d58c6fc2ae
Bradley <liu770807152@hotmail.com>
2021-04-17 17:22:08 +1000

    Added codes for 7th lesson.

---
+++ 7/cache.py
@@ -0,0 +1,36 @@
+import time
+def timeit(f):
+    def timed(*args, **kw):
+        ts = time.time()
+        result = f(*args, **kw)
+        te = time.time()
+        print('func:%r args:[%r, %r] took: %.20f sec' % \
+          (f.__name__, args, kw, te-ts))
+        return result
+    return timed
+
+'''
+还记得我吗？
+'''
+def fib(n: int):
+    if n <= 1:
+        return n
+    return fib(n - 1) + fib(n - 2)
+@timeit
+def printAllSequence(n):
+    for i in range(n):
+        print(i, fib(i))
+print(printAllSequence(30), end='\n\n')
+
+'''
+不用动态规划，能不能改进算法效率？我们可以利用cache！
+'''
+from functools import cache, lru_cache
+# @cache
+# 缓存fib()最近五次运行的结果，存放于cache中
+@lru_cache(maxsize=5)
+def fib(n: int):
+    if n <= 1:
+        return n
+    return fib(n - 1) + fib(n - 2)
+print(printAllSequence(200), end='\n\n')
