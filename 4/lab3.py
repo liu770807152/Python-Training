df0aae4c1c66a74656a4122865ff2550be19bfbe
Bradley <liu770807152@hotmail.com>
2021-04-24 19:14:04 +1000

    Complete codes for 4th lecture.

--- 4/lab3.py
+++ 4/lab3.py
@@ -1,12 +1,24 @@
-def sqrt(n):
+def sqrt_test(n):
     guess = n / 2
     while abs(guess ** 2 - n > 1e-6):
         guess = (guess + n / guess) / 2
     return guess
 
 
+def sqrt(self, x: int) -> int:
+    if x == 0:
+        return 0
+    C, x0 = float(x), float(x)
+    while True:
+        xi = 0.5 * (x0 + C / x0)
+        if abs(x0 - xi) < 1e-7:
+            break
+        x0 = xi
+    return int(x0)
+
+
 if __name__ == '__main__':
     assert(abs(sqrt(4) - 2 <= 1e-6))
     assert(abs(sqrt(10000) - 100 <= 1e-6))
     assert(abs(sqrt(1) - 1 <= 1e-6))
-    assert(abs(sqrt(0) == -1))
+    assert(abs(sqrt(0) == 0))
